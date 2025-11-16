ea954c041a723e0bd9b22585ed295638e308d4a9
Dat120903 <109398945+Dat120903@users.noreply.github.com>
2025-11-16 12:36:27 +0700

    up

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -3,15 +3,8 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 // AUTH
-router.post("/register", authController.register);
-router.post("/login", authController.login);
-router.get("/user/:id", authController.getUser);
-router.put("/update/:id", authController.updateProfile);
-
-
-
-// CART
-router.get("/cart/:userId", authController.getCart);
-router.put("/cart/:userId", authController.updateCart);
+router.post("/register", authController.register);   // đăng ký
+router.post("/login", authController.login);         // đăng nhập
+router.get("/me", authController.me);                // lấy thông tin user từ token
 
 module.exports = router;
